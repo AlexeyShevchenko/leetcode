3dda86700138e9fa7c1f5bc9811327303c3ff89f
Aleksey Shevchenko <alekseiwe@gmail.com>
2021-03-18 22:11:48 +0200

    #206 `Reverse Linked List`

--- leetcode/ViewController.swift
+++ leetcode/ViewController.swift
@@ -11,12 +11,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-
-        let solution = VerifyingAnAlienDictionary.Solution()
-        let result = solution.isAlienSorted(["word", "world", "row"], "worldabcefghijkmnpqstuvxyz")
+        let solution = ReverseLinkedList.Solution()
+        //[1,2,3,4,5]
+        typealias Node = ReverseLinkedList.ListNode
+//        let node5 = Node(5)
+//        let node4 = Node(4, node5)
+//        let node3 = Node(3, node4)
+//        let node2 = Node(2, node3)
+        let node2 = Node(2, nil)
+        let node1 = Node(1, node2)
+        let result = solution.reverseList(node1)
         print(result)
     }
 }
-//["word","world","row"]
-//"worldabcefghijkmnpqstuvxyz"
