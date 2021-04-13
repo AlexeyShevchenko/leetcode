2b3246521d1ef9ce8e2f17823e5fea47fbb2bb17
Aleksey Shevchenko <alekseiwe@gmail.com>
2021-04-13 21:17:35 +0300

    88 refactoring

--- leetcode/#88/MergeSortedArray.swift
+++ leetcode/#88/MergeSortedArray.swift
@@ -10,13 +10,49 @@ import Foundation
 class MergeSortedArray {
     class Solution {
         func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-            let a1 = (m == 0) ? [] : nums1[0...m - 1]
-            let a2 = (n == 0) ? [] : nums2[0...n - 1]
-            var output = [Int]()
-            output += a1
-            output += a2
-            output.sort()
-            nums1 = output
+
+            let arr1 = m == 0 ? [] : nums1[0...m - 1]
+            let arr2 = n == 0 ? [] : nums2[0...n - 1]
+            var index1 = 0
+            var index2 = 0
+            var arr1Ended = false
+            var arr2Ended = false
+            var arr = [Int]()
+
+            while true {
+                if index1 == arr1.count {
+                    arr1Ended = true
+                    break
+                }
+                if index2 == arr2.count {
+                    arr2Ended = true
+                    break
+                }
+
+                let element1 = arr1[index1]
+                let element2 = arr2[index2]
+
+                if element1 > element2 {
+                    arr.append(element2)
+                    index2 = index2 + 1
+                } else if element1 < element2 {
+                    arr.append(element1)
+                    index1 = index1 + 1
+                } else {
+                    arr.append(element1)
+                    arr.append(element2)
+                    index1 = index1 + 1
+                    index2 = index2 + 1
+                }
+            }
+
+            if arr1Ended {
+                arr += arr2[index2...]
+            } else if arr2Ended {
+                arr += arr1[index1...]
+            }
+
+            nums1 = arr
         }
     }
 }
--- leetcode/ViewController.swift
+++ leetcode/ViewController.swift
@@ -12,12 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let solution = MergeSortedArray.Solution()
-        /*[1]
-        1
-        []
-        0
- */
         var num1 = [1]
         solution.merge(&num1, 1, [], 0)
     }
 }
+
+/*
+ Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ Output: [1,2,2,3,5,6]
+ */
