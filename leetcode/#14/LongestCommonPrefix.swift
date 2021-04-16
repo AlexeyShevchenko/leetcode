41c87719eef366268104b155e395e124d0617fc2
Aleksey Shevchenko <alekseiwe@gmail.com>
2021-04-16 12:23:59 +0300

    add #14 Longest Common Prefix https://leetcode.com/problems/longest-common-prefix/

---
+++ leetcode/#14/LongestCommonPrefix.swift
@@ -0,0 +1,16 @@
+//
+//  LongestCommonPrefix.swift
+//  leetcode
+//
+//  Created by Aleksey Shevchenko on 16.04.2021.
+//
+
+import Foundation
+
+class LongestCommonPrefix {
+    class Solution {
+        func longestCommonPrefix(_ strs: [String]) -> String {
+            return ""
+        }
+    }
+}
--- leetcode/ViewController.swift
+++ leetcode/ViewController.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let solution = MergeSortedArray.Solution()
-        var num1 = [1,2,3,0,0,0]
-        solution.merge(&num1, 3, [2,5,6], 3)
+        let solution = LongestCommonPrefix.Solution()
+        let prefix = solution.longestCommonPrefix([""])
+        print(prefix)
     }
 }
