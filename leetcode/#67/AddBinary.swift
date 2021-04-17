c8a8cf6f9c9e0a0bbee75bbad7eb90aa4e818402
Aleksey Shevchenko <alekseiwe@gmail.com>
2021-04-17 18:00:00 +0300

    #67 Add Binary https://leetcode.com/problems/add-binary/

---
+++ leetcode/#67/AddBinary.swift
@@ -0,0 +1,16 @@
+//
+//  AddBinary.swift
+//  leetcode
+//
+//  Created by Aleksey Shevchenko on 17.04.2021.
+//
+
+import Foundation
+
+class AddBinary {
+    class Solution {
+        func addBinary(_ a: String, _ b: String) -> String {
+            return ""
+        }
+    }
+}
