1358c305942f00bbce9c197d17069dda9870baf1
Aleksey Shevchenko <alekseiwe@gmail.com>
2021-04-19 18:36:11 +0300

    one more attempt

--- leetcode/#67/AddBinary.swift
+++ leetcode/#67/AddBinary.swift
@@ -10,56 +10,32 @@ import Foundation
 class AddBinary {
     class Solution {
         func addBinary(_ a: String, _ b: String) -> String {
-            var aToUse = a
-            var bToUse = b
-            let numberOfZeros = abs(aToUse.count - bToUse.count)
-            if numberOfZeros > 0 {
-                let zeros = [String](repeating: "0", count: numberOfZeros).joined()
-                if aToUse.count < bToUse.count {
-                    aToUse = zeros + aToUse
-                } else {
-                    bToUse = zeros + bToUse
-                }
-            }
-            var res = ""
+            let aArray = Array(a)
+            let bArray = Array(b)
+            var i = aArray.count - 1
+            var j = bArray.count - 1
             var carry = 0
-            for i in stride(from: aToUse.count - 1, through: 0, by: -1) {
-                let fromA = aToUse[i]
-                let fromB = bToUse[i]
-                if fromA == fromB {
-                    // 0 and 0
-                    if fromA == "0" {
-                        if carry > 0 {
-                            res.insert(contentsOf: "1", at: res.startIndex)
-                            carry = 0
-                        } else {
-                            res.insert(contentsOf: "0", at: res.startIndex)
-                            carry = 0
-                        }
-                    // 1 and 1
-                    } else {
-                        if carry > 0 {
-                            res.insert(contentsOf: "1", at: res.startIndex)
-                            carry = 1
-                        } else {
-                            res.insert(contentsOf: "0", at: res.startIndex)
-                            carry = 1
-                        }
+            var result = ""
+
+            while i >= 0 || j >= 0 || carry > 0 {
+                var sum = carry
+                if i >= 0 {
+                    if let num = Int(String(aArray[i])) {
+                        sum += num
                     }
-                } else { // '0 and 1' or '1 and 0'
-                    if carry > 0 {
-                        res.insert(contentsOf: "0", at: res.startIndex)
-                        carry = 1
-                    } else {
-                        res.insert(contentsOf: "1", at: res.startIndex)
-                        carry = 0
-                    }
+                    i -= 1
                 }
+                if j >= 0 {
+                    if let num = Int(String(bArray[j])) {
+                        sum += num
+                    }
+                    j -= 1
+                }
+                result = "\(sum % 2)" + result
+                carry = sum / 2
             }
-            if carry > 0 {
-                res.insert("1", at: res.startIndex)
-            }
-            return res
+
+            return result
         }
     }
 }
