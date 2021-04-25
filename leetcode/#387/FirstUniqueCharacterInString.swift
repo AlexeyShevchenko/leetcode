ddb9b9bed401834ed23ce70f5081be2e0419f2c7
Aleksey Shevchenko <alekseiwe@gmail.com>
2021-04-25 13:38:35 +0300

    less code

--- leetcode/#387/FirstUniqueCharacterInString.swift
+++ leetcode/#387/FirstUniqueCharacterInString.swift
@@ -12,16 +12,12 @@ class FirstUniqueCharacterInString {
         func firstUniqChar(_ s: String) -> Int {
             var dict = [String.Element: Int]()
             for (_, element) in s.enumerated() {
-                if let val = dict[element] {
-                    dict[element] = val + 1
-                } else {
-                    dict[element] = 0
-                }
+                guard let val = dict[element] else { dict[element] = 0; continue }
+                dict[element] = val + 1
             }
             for (index, element) in s.enumerated() {
-                if let val = dict[element], val == 0 {
-                    return index
-                }
+                guard let value = dict[element], value == 0 else { continue }
+                return index
             }
             return -1
         }
