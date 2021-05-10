0a7cdd73d037eb44c3db38642ce64ac84aede4aa
Aleksey Shevchenko <alekseiwe@gmail.com>
2021-05-10 23:49:27 +0300

    #101 Symmetric Tree https://leetcode.com/problems/symmetric-tree/

---
+++ leetcode/#101/SymmetricTree.swift
@@ -0,0 +1,25 @@
+//
+//  SymmetricTree.swift
+//  leetcode
+//
+//  Created by Aleksey Shevchenko on 10.05.2021.
+//
+
+import Foundation
+
+class SymmetricTree {
+
+    typealias TreeNode = SameTree.TreeNode
+
+    class Solution {
+        func isSymmetric(_ root: TreeNode?) -> Bool {
+            return isMirror(root, root)
+        }
+
+        private func isMirror(_ left: TreeNode?, _ right: TreeNode?) -> Bool {
+            if left == nil && right == nil { return true }
+            if left == nil || right == nil { return false }
+            return left?.val == right?.val && isMirror(left?.left, right?.right) && isMirror(left?.right, right?.left)
+        }
+    }
+}
