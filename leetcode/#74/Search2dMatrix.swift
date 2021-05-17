b44868df063436978921c493eee8fbc2fd8bb6ae
Aleksey Shevchenko <alekseiwe@gmail.com>
2021-05-17 18:39:24 +0300

    try to improve

--- leetcode/#74/Search2dMatrix.swift
+++ leetcode/#74/Search2dMatrix.swift
@@ -10,11 +10,42 @@ import Foundation
 class Search2dMatrix {
     class Solution {
         func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
-            for m in 0..<matrix.count {
-                for n in 0..<matrix[m].count {
-                    let cell = matrix[m][n]
-                    if cell == target { return true }
+            if matrix.count < 1 { return false }
+            if matrix.count == 1 {
+                let row = matrix[0]
+                return binarySearch(in: row, target: target)
+            }
+
+            var index = 0
+            var nextIndex = 1
+
+            while nextIndex < matrix.count {
+                let firstElementInRow = matrix[index].first ?? 0
+                let nextFirstElementInRow = matrix[nextIndex].first ?? 0
+
+                if target >= firstElementInRow && target < nextFirstElementInRow {
+                    let row = matrix[index]
+                    return binarySearch(in: row, target: target)
+                } else if target == nextFirstElementInRow {
+                    return true
                 }
+                
+                index += 1
+                nextIndex += 1
+            }
+
+            return binarySearch(in: matrix[matrix.count - 1], target: target)
+        }
+
+        private func binarySearch(in row: [Int], target: Int) -> Bool {
+            var left = 0
+            var rigth = row.count - 1
+            while left <= rigth {
+                let middle = (left + rigth) / 2
+                let numAtMiddle = row[middle]
+                if target == numAtMiddle { return true }
+                else if target < numAtMiddle { rigth = middle - 1 }
+                else if target > numAtMiddle { left = middle + 1 }
             }
             return false
         }
--- leetcode/ViewController.swift
+++ leetcode/ViewController.swift
@@ -16,15 +16,37 @@ class ViewController: UIViewController {
             [
                 [1,3,5,7],
                 [10,11,16,20],
-                [23,30,34,60]
-            ], 3
+                [23,30,34,60],
+            ], 13
         )
         print(exist)
+        let exist1 = solution.searchMatrix(
+            [
+                [1],
+            ], -1
+        )
+        print(exist1)
+        let exist2 = solution.searchMatrix(
+            [
+                [1],
+                [3],
+            ], -3
+        )
+        print(exist2)
+        let exist3 = solution.searchMatrix(
+            [
+                [1,3,5,7],
+                [10,11,16,20],
+                [23,30,34,50],
+            ],
+            30
+        )
+        print(exist3)
     }
 }
 
 class QSort {
-    
+
     func sort<T: Comparable>(_ array: [T]) -> [T] {
         guard array.count > 1 else { return array }
         let pivot = array[array.count / 2]
