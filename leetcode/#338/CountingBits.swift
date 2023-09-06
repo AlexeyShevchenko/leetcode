e8f57286b2a5f0c7bbe0ab345ba17f6bdc2afcc4
Aleksey Shevchenko <alekseiwe@gmail.com>
2023-09-06 17:25:02 +0100

    leetcode-338: Counting Bits

--- leetcode/#338/CountingBits.swift
+++ leetcode/#338/CountingBits.swift
@@ -10,41 +10,15 @@ import Foundation
 class CountingBits {
     class Solution {
         func countBits(_ n: Int) -> [Int] {
-            var result: [Int] = []
+            var result: [Int] = Array(repeating: 0, count: n + 1)
+            
             for i in 0 ... n {
-                let c = hammingWeight(i)
-                result.append(c)
+                result[i] = result[i >> 1] + i & 1
             }
             
             return result
         }
-        
-        private func hammingWeight(_ num: Int) -> Int {
-            var n = num
-            var numberOf1Bits = 0
-            
-            while n > 0 {
-                if n & 1 == 1 {
-                    numberOf1Bits += 1
-                }
-                n = n >> 1
-            }
-            
-            return numberOf1Bits
-        }
     }
 }
-
-/*
- 
- Input: n = 5
- Output: [0,1,1,2,1,2]
- Explanation:
- 0 --> 0
- 1 --> 1
- 2 --> 10
- 3 --> 11
- 4 --> 100
- 5 --> 101
- 
- */
+// Time complexity O(n)
+// Space complexity O(n)
