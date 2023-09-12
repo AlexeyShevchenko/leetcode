31c6bb5927a2c4a0072c5d81f62f44c7e2564af4
Aleksey Shevchenko <alekseiwe@gmail.com>
2023-09-12 09:59:38 +0100

    leetcode-128: Longest Consecutive Sequence

---
+++ leetcode/#128/LongestConsecutiveSequence.swift
@@ -0,0 +1,33 @@
+//
+//  LongestConsecutiveSequence.swift
+//  leetcode
+//
+//  Created by Aleksey Shevchenko on 12.09.2023.
+//
+
+import Foundation
+
+class LongestConsecutiveSequence {
+    class Solution {
+        func longestConsecutive(_ nums: [Int]) -> Int {
+            let set: Set<Int> = .init(nums)
+            var maxLenght = 0
+            
+            for num in nums {
+                let previousConsecutiveNum = num - 1
+                if set.contains(previousConsecutiveNum) { continue }
+                
+                var currentMaxLenght = 1
+                var nextConsecutiveNum = num + 1
+                while set.contains(nextConsecutiveNum) {
+                    nextConsecutiveNum += 1
+                    currentMaxLenght += 1
+                }
+                
+                maxLenght = max(maxLenght, currentMaxLenght)
+            }
+            
+            return maxLenght
+        }
+    }
+}
