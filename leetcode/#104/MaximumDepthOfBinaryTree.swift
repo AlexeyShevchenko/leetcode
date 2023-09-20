85bba2ba4c64e5cf9f95b6131380f602b95898fd
Aleksey Shevchenko <alekseiwe@gmail.com>
2023-09-20 21:38:04 +0100

    leetcode-104: Maximum Depth of Binary Tree

--- leetcode/#104/MaximumDepthOfBinaryTree.swift
+++ leetcode/#104/MaximumDepthOfBinaryTree.swift
@@ -7,36 +7,16 @@
 
 import Foundation
 
-public class TreeNode {
-
-    public var val: Int
-    public var left: TreeNode?
-    public var right: TreeNode?
-
-    public init() { self.val = 0; self.left = nil; self.right = nil; }
-
-    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-
-    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-        self.val = val
-        self.left = left
-        self.right = right
-    }
-}
-
-/// https://leetcode.com/problems/maximum-depth-of-binary-tree/
 class MaximumDepthOfBinaryTree {
-
     class Solution {
-
         func maxDepth(_ root: TreeNode?) -> Int {
-
-            //Input: root = [3,9,20,null,null,15,7]
-            // Output: 3
-            //      3
-            //     9 20
-
-            return 0
+            if root == nil { return 0 }
+            let maxLeft = maxDepth(root?.left)
+            let maxRight = maxDepth(root?.right)
+            return max(maxLeft, maxRight) + 1
         }
     }
 }
+
+// Time complexity: O(n)
+// Space complexity: O(n)
---
+++ leetcode/TreeNode.swift
@@ -0,0 +1,24 @@
+//
+//  TreeNode.swift
+//  leetcode
+//
+//  Created by Aleksey Shevchenko on 20.09.2023.
+//
+
+import Foundation
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
