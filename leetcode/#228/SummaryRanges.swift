70eda1c7fbf5306ebd91e788facf56847e9b0581
Aleksey Shevchenko <alekseiwe@gmail.com>
2023-10-13 13:21:49 +0100

    leetcode-228: Summary Ranges

---
+++ leetcode/#228/SummaryRanges.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryRanges.swift
+//  leetcode
+//
+//  Created by Aleksey Shevchenko on 13.10.2023.
+//
+
+import Foundation
+
+class SummaryRanges {
+    class Solution {
+        func summaryRanges(_ nums: [Int]) -> [String] {
+            
+            
+            return []
+        }
+    }
+}
