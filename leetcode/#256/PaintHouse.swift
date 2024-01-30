146121269deeea015f6298ce113477879aa1d2d6
Aleksey Shevchenko <alekseiwe@gmail.com>
2024-01-30 15:21:53 +0000

    leetcode-256: Paint House

--- leetcode/#256/PaintHouse.swift
+++ leetcode/#256/PaintHouse.swift
@@ -13,10 +13,10 @@ class PaintHouse {
             var costs = costs
             
             for i in 1 ..< costs.count {
-                let prevHoustPaintCost = costs[i - 1]
-                costs[i][0] = costs[i][0] + min(prevHoustPaintCost[1], prevHoustPaintCost[2])
-                costs[i][1] = costs[i][1] + min(prevHoustPaintCost[0], prevHoustPaintCost[2])
-                costs[i][2] = costs[i][2] + min(prevHoustPaintCost[0], prevHoustPaintCost[1])
+                let prevHousePaintCost = costs[i - 1]
+                costs[i][0] = costs[i][0] + min(prevHousePaintCost[1], prevHousePaintCost[2])
+                costs[i][1] = costs[i][1] + min(prevHousePaintCost[0], prevHousePaintCost[2])
+                costs[i][2] = costs[i][2] + min(prevHousePaintCost[0], prevHousePaintCost[1])
             }
             
             return costs[costs.count - 1].min() ?? 0
