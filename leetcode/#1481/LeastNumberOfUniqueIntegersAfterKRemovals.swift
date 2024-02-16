fd280ebae3c6e4c67eb7cdcbfaba6dcb7b62b073
Aleksey Shevchenko <alekseiwe@gmail.com>
2024-02-16 12:55:28 +0000

    leetcode-1481: Least Number of Unique Integers after K Removals

---
+++ leetcode/#1481/LeastNumberOfUniqueIntegersAfterKRemovals.swift
@@ -0,0 +1,20 @@
+//
+//  LeastNumberOfUniqueIntegersAfterKRemovals.swift
+//  leetcode
+//
+//  Created by Aleksey Shevchenko on 16.02.2024.
+//
+
+import Foundation
+
+class LeastNumberOfUniqueIntegersAfterKRemovals {
+    class Solution {
+        func findLeastNumOfUniqueInts(_ arr: [Int], _ k: Int) -> Int {
+            var res = 0
+            
+            
+            
+            return 0
+        }
+    }
+}
