3fc421d5575947937c6493e74efd2fc9adfe73e8
Aleksey Shevchenko <alekseiwe@gmail.com>
2024-02-16 13:49:18 +0000

    leetcode-1481: Least Number of Unique Integers after K Removals

--- leetcode/#1481/LeastNumberOfUniqueIntegersAfterKRemovals.swift
+++ leetcode/#1481/LeastNumberOfUniqueIntegersAfterKRemovals.swift
@@ -10,11 +10,42 @@ import Foundation
 class LeastNumberOfUniqueIntegersAfterKRemovals {
     class Solution {
         func findLeastNumOfUniqueInts(_ arr: [Int], _ k: Int) -> Int {
+            var k = k
             var res = 0
+            var numsAndFrequency: [Int: Int] = [:]
+            for i in 0 ..< arr.count {
+                let num = arr[i]
+                if let frequency = numsAndFrequency[num] {
+                    numsAndFrequency[num] = frequency + 1
+                } else {
+                    numsAndFrequency[num] = 1
+                }
+            }
             
+            let sortedNumsByFrequency = numsAndFrequency.sorted(by: { $0.value < $1.value })
             
+            for pair in sortedNumsByFrequency.enumerated() {
+                if k == 0 { break }
+                
+                let frequency = pair.element.value
+                let num = pair.element.key
+                if frequency >= k {
+                    numsAndFrequency[num] = frequency - k
+                    k = 0
+                } else {
+                    numsAndFrequency[num] = 0
+                    k -= frequency
+                }
+            }
             
-            return 0
+            for key in numsAndFrequency.keys {
+                guard let frequency = numsAndFrequency[key] else { continue }
+                if frequency > 0 {
+                    res += 1
+                }
+            }
+            
+            return res
         }
     }
 }
--- leetcode/ViewController.swift
+++ leetcode/ViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        let s = SortCharactersByFrequency.Solution()
-        let r = s.frequencySort("tree")
+        let s = LeastNumberOfUniqueIntegersAfterKRemovals.Solution()
+        let r = s.findLeastNumOfUniqueInts([4,3,1,1,3,3,2], 3)
         print(r)
     }
 }
