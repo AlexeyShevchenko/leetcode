6f81ea7539279b96427cb5d27e2ab596d89a83eb
Aleksey Shevchenko <alekseiwe@gmail.com>
2024-06-21 15:47:47 +0100

    leetcode-1052: Grumpy Bookstore Owner

---
+++ leetcode/#1052/GrumpyBookstoreOwner.swift
@@ -0,0 +1,43 @@
+//
+//  GrumpyBookstoreOwner.swift
+//  leetcode
+//
+//  Created by Aleksey Shevchenko on 21.06.2024.
+//
+
+import Foundation
+
+final class GrumpyBookstoreOwner {
+    class Solution {
+        func maxSatisfied(
+            _ customers: [Int],
+            _ grumpy: [Int],
+            _ minutes: Int
+        ) -> Int {
+            var left = 0
+            var window = 0
+            var maxWindow = 0
+            var satisfied = 0
+            
+            for right in 0 ..< customers.count {
+                if grumpy[right] == 1 {
+                    window += customers[right]
+                } 
+                else {
+                    satisfied += customers[right]
+                }
+                
+                if right - left + 1 > minutes {
+                    if grumpy[left] == 1 {
+                        window -= customers[left]
+                    }
+                    left += 1
+                }
+                
+                maxWindow = max(maxWindow, window)
+            }
+            
+            return satisfied + maxWindow
+        }
+    }
+}
