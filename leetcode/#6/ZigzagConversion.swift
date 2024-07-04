f312575db6e2d3b2a6453e4d33f1fbfca17f1721
Aleksey Shevchenko <alekseiwe@gmail.com>
2024-07-04 16:17:18 +0100

    leetcode-6: Zigzag Conversion

---
+++ leetcode/#6/ZigzagConversion.swift
@@ -0,0 +1,42 @@
+//
+//  ZigzagConversion.swift
+//  leetcode
+//
+//  Created by Aleksey Shevchenko on 04.07.2024.
+//
+
+import Foundation
+
+class ZigzagConversion {
+    class Solution {
+        func convert(_ s: String, _ numRows: Int) -> String {
+            if numRows == 1 { return s }
+            if numRows >= s.count { return s }
+            
+            var rows: [[Character]] = []
+            for i in 0 ..< numRows {
+                rows.append([])
+            }
+            
+            var currentRowIndex = 0
+            var step = 1
+            
+            for char in s {
+                rows[currentRowIndex].append(char)
+                currentRowIndex += step
+                
+                if currentRowIndex == 0 || currentRowIndex == numRows - 1 {
+                    step = -step
+                }
+            }
+            
+            var result = ""
+            for i in 0 ..< rows.count {
+                let row = rows[i]
+                result.append(.init(row))
+            }
+            
+            return result
+        }
+    }
+}
